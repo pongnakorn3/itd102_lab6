904844f8e8eace99e528ed7064891c3e58a5233d
pongnakorn3 <pongnakorn3@gmail.com>
2024-12-13 12:32:53 +0700

    lab7

--- server.js
+++ server.js
@@ -4,6 +4,12 @@ const express = require('express');
 const app = express();
 const PORT = process.env.APP_PORT || 5000;
 
+// Db connection
+const db = require('./app/models');
+db.sequelize.sync({force: false}).then(()=>{
+    console.log("DB Connected!");
+});
+
 // Routes
 require('./app/routes/user.route')(app);
 
